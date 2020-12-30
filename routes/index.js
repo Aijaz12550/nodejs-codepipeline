08d40201e249255006d688cbe45f4723753714a4
Aijaz12550 <muhammadaijaz76@gmail.com>
2020-12-30 17:21:23 +0500

    second commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'AWS CodePipeline' });
+  res.render('index', { title: 'AWS CodePipeline Done' });
 });
 
 module.exports = router;
